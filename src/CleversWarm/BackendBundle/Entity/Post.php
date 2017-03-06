440a6cfb9d7ec9e6c0162a4312712a2568e49e10
lartist <s.benouamer@hotmail.fr>
2017-03-06 07:38:59 +0100

    création de l'entité post

---
+++ src/CleversWarm/BackendBundle/Entity/Post.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace CleversWarm\BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="CleversWarm\BackendBundle\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="posted", type="datetime")
+     */
+    private $posted;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", unique=true)
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated", type="datetime", nullable=true)
+     */
+    private $updated;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="enabled", type="integer")
+     */
+    private $enabled;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Post
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set posted
+     *
+     * @param \DateTime $posted
+     *
+     * @return Post
+     */
+    public function setPosted($posted)
+    {
+        $this->posted = $posted;
+
+        return $this;
+    }
+
+    /**
+     * Get posted
+     *
+     * @return \DateTime
+     */
+    public function getPosted()
+    {
+        return $this->posted;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     *
+     * @return Post
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param integer $enabled
+     *
+     * @return Post
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return int
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+}
+
